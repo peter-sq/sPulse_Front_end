41b0b3c143e66a67df203e26b3a06c8e70e65f6a
peter-sq <awe.petersq@gmail.com>
2024-07-20 19:07:50 +0100

    create game fix

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -1,99 +1,144 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Table, { AvatarCell, SelectColumnFilter, StatusPill } from './Table';
-import {Link} from 'react-router-dom'
+import { ClipLoader } from 'react-spinners';
+import { Link } from 'react-router-dom';
+import SuccessAlert from '../../shared/Modals/sucessAlert';
+import ErrorAlert from '../../shared/Modals/errorAlert';
 
-const getData = () => {
-  const data = [
-    {
-      id: 1,
-      name: 'Jane Cooper',
-      email: 'jane.cooper@example.com',
-      title: 'Regional Paradigm Technician',
-      department: 'Optimization',
-      status: 'Active',
-      role: 'Admin',
-      age: 27,
-      imgUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    {
-      id: 2,
-      name: 'Cody Fisher',
-      email: 'cody.fisher@example.com',
-      title: 'Product Directives Officer',
-      department: 'Intranet',
-      status: 'Inactive',
-      role: 'Owner',
-      age: 43,
-      imgUrl: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-  ];
-  return [...data, ...data, ...data].map((item, index) => ({ ...item, id: index + 1 }));
-};
 
-const ActionButtons = ({ row, onEdit, onDelete, onView }) => {
+// Define Base Url
+const BASE_URL = process.env.REACT_APP_API_URL;
+
+const ActionButtons = ({ row, onDelete }) => {
   return (
     <div className="flex space-x-1">
-      <Link to = '/dashboard/create-game'>
-         <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-      >
-        View
-      </button>
+      <Link to='/dashboard/create-game'>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+          View
+        </button>
       </Link>
       <button
-        className="bg-red hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
         onClick={() => onDelete(row.original)}
+        className="bg-red hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
       >
         Delete
       </button>
-   
     </div>
   );
 };
 
 function Home() {
+  // Define our states
+  const [loading, setLoading] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+  const [datas, setDatas] = useState([]);
 
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/api/v1/user/all-games`)
+        setDatas(response.data);
+      } catch (error) {
+        console.log('error fetching data', error)
+        
+      }
+    };
+    fetchGames();
+  }, [])
+
+  // Handle Add Game Button
+  const AddGame = async () => {
+    const data = {
+      date: new Date().toISOString(),
+    };
+    setLoading(true);
+    setShowAlert(false);
+
+    const MINIMUM_LOADING_TIME = 1000; 
+    const startTime = Date.now();
+
+    try {
+      const response = await axios.post(`${BASE_URL}/api/v1/admin/post/create-game`, data);
+      console.log('Response:', response);
+
+      const elapsedTime = Date.now() - startTime;
+      const remainingTime = Math.max(0, MINIMUM_LOADING_TIME - elapsedTime);
+
+      setTimeout(() => {
+        console.log('Setting success message');
+        setAlertMessage('Game Created successfully!');
+        setShowAlert(true);
+      }, remainingTime);
+    } catch (error) {
+      console.log('Error:', error);
+      
+      const elapsedTime = Date.now() - startTime;
+      const remainingTime = Math.max(0, MINIMUM_LOADING_TIME - elapsedTime);
+
+      setTimeout(() => {
+        console.log('Setting error message');
+        setAlertMessage('Failed to submit data!');
+        setShowAlert(true);
+      }, remainingTime);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  // Handle Delete Game Button
+  const handleDelete = async (game) => {
+    setLoading(true);
+    try {
+      await axios.delete(`${BASE_URL}/api/v1/admin/post/delete-game/${game._id}`);
+      setDatas((prevData) => prevData.filter((item) => item._id !== game._id));
+      setAlertMessage('Game deleted successfully!');
+      setShowAlert(true);
+    } catch (error) {
+      console.log("There is an error", error);
+      ErrorAlert('Failed to delete game!');
+      ErrorAlert(true);
+    }finally{
+      setLoading(false);
+    }
+  };
 
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Name',
-        accessor: 'name',
-        Cell: AvatarCell,
-        imgAccessor: 'imgUrl',
-        emailAccessor: 'email',
+        Header: 'S/N',
+        accessor: (row, i) => i + 1,
       },
       {
-        Header: 'Title',
-        accessor: 'title',
+        Header: 'ID',
+        accessor: 'game_id',
       },
       {
-        Header: 'Status',
-        accessor: 'status',
-        Cell: StatusPill,
-      },
-      {
-        Header: 'Age',
-        accessor: 'age',
-      },
-      {
-        Header: 'Role',
-        accessor: 'role',
-        Filter: SelectColumnFilter,
-        filter: 'includes',
+        Header: 'Date',
+        accessor: 'date',
+        Cell: ({ value }) => {
+          const formattedDate = new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+          }).format(new Date(value));
+          return formattedDate;
+        },
       },
       {
         Header: 'Action',
         accessor: 'action',
         Cell: ({ row }) => (
-          <ActionButtons row={row}  />
+          <ActionButtons row={row} onDelete={handleDelete} />
         ),
       },
     ],
     []
   );
 
-  const data = React.useMemo(() => getData(), []);
+  
 
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
@@ -101,15 +146,23 @@ function Home() {
         <div>
           <h1 className="text-xl font-semibold">
             <button
+              onClick={AddGame}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+              disabled={loading}
             >
-              Add a New Game
+              {loading ? <ClipLoader size={20} color="#ffffff" /> : 'Add a New Game'}
             </button>
           </h1>
         </div>
         <div className="mt-6">
-          <Table columns={columns} data={data} />
+          <Table columns={columns} data={datas} />
         </div>
+        {showAlert && (
+          <SuccessAlert
+            message={alertMessage}
+            onClose={() => setShowAlert(false)}
+          />
+        )}
       </main>
     </div>
   );
