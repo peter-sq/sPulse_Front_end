4025f98182a7228e1442d558aff640ea52321be5
peter-sq <awe.petersq@gmail.com>
2024-07-25 12:07:17 +0100

    refactor users view

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -33,7 +33,7 @@ const Hero = () => {
 
             <ul className="mt-12 space-y-6">
                 {DailyPrediction.map((item) => (
-                    <li key={item._id} className="p-5 bg-white rounded-md shadow-sm">
+                    <li key={item._id} className="p-5 bg-white rounded-[1rem] shadow-sm">
                         <div>
                             <div className="justify-between sm:flex">
                                 <div className="flex-1">
@@ -41,25 +41,25 @@ const Hero = () => {
                                 </div>
                             </div>
                             <div className="mt-4 items-center space-y-4 text-sm  sm:space-x-4 sm:space-y-0">
-                                <div className="overflow-x-auto">
-                                    <table className="min-w-full divide-y divide-gray-200 table-fixed">
-                                        <thead>
+                                        <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
+                                    <table className="w-full table-auto text-sm text-left">
+                                        <thead className=" bg-red text-gray-600 font-medium border-b">
                                             <tr>
-                                                <th className="px-6 py-3 bg-gray-50 text-center text-[16px] font-medium text-gray-500 uppercase tracking-wider">Time</th>
-                                                <th className="px-6 py-3 bg-gray-50 text-center text-[16px] font-medium text-gray-500 uppercase tracking-wider">Fixtures</th>
-                                                <th className="px-6 py-3 bg-gray-50 text-center text-[16px] font-medium text-gray-500 uppercase tracking-wider">Odds</th>
-                                                <th className="px-6 py-3 bg-gray-50 text-center text-[16px] font-medium text-gray-500 uppercase tracking-wider">Prediction</th>
-                                                <th className="px-6 py-3 bg-gray-50 text-center text-[16px] font-medium text-gray-500 uppercase tracking-wider">Result</th>
+                                                <th className="px-6 py-3 bg-gray-50 text-[14px] font-bold text-white leading-4 uppercase tracking-wide">Time</th>
+                                                <th className="px-6 py-3 bg-gray-50  text-[14px] font-bold text-white leading-4 uppercase tracking-wide">Fixtures</th>
+                                                <th className="px-6 py-3 bg-gray-50  text-[14px] font-bold text-white leading-4 uppercase tracking-wide">Odds</th>
+                                                <th className="px-6 py-3 bg-gray-50  text-[14px] font-bold text-white leading-4 uppercase tracking-wide">Prediction</th>
+                                                <th className="px-6 py-3 bg-gray-50  text-[14px] font-bold text-white leading-4 uppercase tracking-wide">Result</th>
                                             </tr>
                                         </thead>
-                                        <tbody className="bg-white divide-y divide-gray-200">
+                                        <tbody className="bg-white divide-y divide-gray-600">
                                             {item.predictions.map((prediction) => (
                                                 <tr key={prediction._id}>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] font-Roboto text-gray-500">{prediction.time}</td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] font-Roboto text-gray-500">{prediction.fixtures}</td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] font-Roboto text-gray-500">{prediction.odds}</td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] font-Roboto text-gray-500">{prediction.prediction}</td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] font-Roboto text-gray-500">{prediction.result}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] capitalize  font-Roboto text-gray-500">{prediction.time}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] capitalize  font-Roboto text-gray-500">{prediction.fixtures}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] capitalize  font-Roboto text-gray-500">{prediction.odds}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] capitalize  font-Roboto text-gray-500">{prediction.prediction}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-[16px] capitalize font-Roboto text-gray-500">{prediction.result}</td>
                                                 </tr>
                                             ))}
                                         </tbody>
